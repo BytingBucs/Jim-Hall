badba01ddbd4f4c02a138a0b80b82ad9d718b36a
Sudent Account K-127 <LE-K127-ST@FSW.EDU>
2016-02-11 16:06:55 -0500

    update

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,15 +1,38 @@
 var main = function () {
 	"use strict";
-
+			
 	$(".comment-input button").on("click", function (event)
+
 	{
 		var $new_comment; 
 
 		if ($(".comment-input input").val() !== "") 
 		{
 			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
 			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
 		}
+	});
+
+	$(".comment-input input").on("keypress", function (event){
+		if (event.keyCode === 13) {
+			console.log("this is the keyCode" + event.keyCode);
+		var $new_comment; 
+
+		if ($(".comment-input input").val() !== "") 
+		{
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
+		}
+	}
+
+
+		
 	});
 
 };
