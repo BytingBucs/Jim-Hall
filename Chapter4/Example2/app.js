075aa4cc46a0ec0a1e8e57940065e8bc8964a63e
jkrhall3 <jimbyy@creativethoughts.net>
2016-02-17 15:31:39 -0500

    refactor complete app still works

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,9 +1,9 @@
 var main = function () {
 	"use strict";
 			
-	$(".comment-input button").on("click", function (event)
+	
 
-	{
+	var addCommentFromInputBox = function () {
 		var $new_comment; 
 
 		if ($(".comment-input input").val() !== "") 
@@ -14,24 +14,16 @@ var main = function () {
 			$new_comment.fadeIn();
 			$(".comment-input input").val("");
 		}
+	};
+
+	$(".comment-input button").on("click", function (event){
+		addCommentFromInputBox();
 	});
 
 	$(".comment-input input").on("keypress", function (event){
 		if (event.keyCode === 13) {
-			console.log("this is the keyCode" + event.keyCode);
-		var $new_comment; 
-
-		if ($(".comment-input input").val() !== "") 
-		{
-			$new_comment = $("<p>").text($(".comment-input input").val());
-			$new_comment.hide();
-			$(".comments").append($new_comment);
-			$new_comment.fadeIn();
-			$(".comment-input input").val("");
+			addCommentFromInputBox();
 		}
-	}
-
-
 		
 	});
 
