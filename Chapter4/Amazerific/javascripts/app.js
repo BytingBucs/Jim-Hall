085d4e01833078dfeaa797f03575caab01d1ce39
jkrhall3 <jimbyy@creativethoughts.net>
2016-02-17 22:01:18 -0500

    adding content... doesnt seem to work.. pushing on

--- Chapter4/Amazerific/javascripts/app.js
+++ Chapter4/Amazerific/javascripts/app.js
@@ -1,12 +1,26 @@
 var main = function () {
 	"use strict";
 
-	$(".tabs span").toArray().forEach(function (element) {
+	
+
+	$(".tabs a span").toArray().forEach(function (element) {
 		// create a click handler for this element
 		$(element).on("click",function() {
-			$(".tabs span").removeClass("active");
-			$(element).addClass("active");
+			
+			var $element = $(element);
+
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
+			
+			if ($element.parent().is (":nth-child(1)")) {
+				console.log("First Tab Clicked!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Second Tab Clicked!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("THIRD TAB CLICKED!");
+			}
+
 			return false;
 
 		});
