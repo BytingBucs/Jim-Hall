ad05e6b91a977ab8d2157c38e174db709394d06b
jkrhall3 <jimbyy@creativethoughts.net>
2016-02-17 21:36:02 -0500

    Another Refactor - Seems to work - Less Understood

--- Chapter4/Amazerific/javascripts/app.js
+++ Chapter4/Amazerific/javascripts/app.js
@@ -1,29 +1,16 @@
 var main = function () {
 	"use strict";
 
-	var makeTabActive = function (tabNumber){
-		// construct the selector from the tabNumber
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(".tabs span").removeClass("active");
-		$(tabSelector).addClass("active");
-		$("main .content").empty();
-		return false;
-	}
+	$(".tabs span").toArray().forEach(function (element) {
+		// create a click handler for this element
+		$(element).on("click",function() {
+			$(".tabs span").removeClass("active");
+			$(element).addClass("active");
+			$("main .content").empty();
+			return false;
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		makeTabActive(1);
-		return false;
+		});
 
-	});
-
-	$(".tabs a:nth-child(2)").on("click", function (){
-		makeTabActive(2);
-		return false;
-	});
-
-	$(".tabs a:nth-child(3)").on("click", function (){
-		makeTabActive(3);
-		return false;
 	});
 
 };
