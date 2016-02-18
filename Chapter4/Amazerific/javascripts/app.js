172916b68e744664b2b9a83bb582bec9262ee831
jkrhall3 <jimbyy@creativethoughts.net>
2016-02-17 23:54:49 -0500

    Working after much code scouring

--- Chapter4/Amazerific/javascripts/app.js
+++ Chapter4/Amazerific/javascripts/app.js
@@ -1,32 +1,54 @@
 var main = function () {
 	"use strict";
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
 
-	
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $submit,
+                backward;
 
-	$(".tabs a span").toArray().forEach(function (element) {
-		// create a click handler for this element
-		$(element).on("click",function() {
-			
-			var $element = $(element);
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
 
-			$(".tabs a span").removeClass("active");
-			$element.addClass("active");
-			$("main .content").empty();
-			
-			if ($element.parent().is (":nth-child(1)")) {
-				console.log("First Tab Clicked!");
-			} else if ($element.parent().is(":nth-child(2)")) {
-				console.log("Second Tab Clicked!");
-			} else if ($element.parent().is(":nth-child(3)")) {
-				console.log("THIRD TAB CLICKED!");
-			}
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                for (backward = toDos.length-1; backward >= 0; backward--) {
+                    $content.append($("<li>").text(toDos[backward]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                $input = $("<input>"),
+                $submit = $("<submit>").text("Submit");
+                $submit.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+                $content = $("<div>").append($input).append($submit);
+         
+            }
 
-			return false;
+            $("main .content").append($content);
+            return false;
+        });
+    });
 
-		});
-
-	});
-
+    $(".tabs a:first-child span").trigger("click");
 };
 
-$(document).ready(main); 
+$(document).ready(main);
