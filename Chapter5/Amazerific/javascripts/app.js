108bf7753faf1e7d678dcae7f62a4163bec030fe
jkrhall3 <jimbyy@creativethoughts.net>
2016-03-02 23:44:52 -0500

    Made a little progress finally had to skip the browser problem its sure to rear its ugly head again later

---
+++ Chapter5/Amazerific/javascripts/app.js
@@ -0,0 +1,54 @@
+var main = function () {
+	"use strict";
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $submit,
+                backward;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                for (backward = toDos.length-1; backward >= 0; backward--) {
+                    $content.append($("<li>").text(toDos[backward]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                $input = $("<input>"),
+                $submit = $("<submit>").text("Submit");
+                $submit.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+                $content = $("<div>").append($input).append($submit);
+         
+            }
+
+            $("main .content").append($content);
+            return false;
+        });
+    });
+
+    $(".tabs a:first-child span").trigger("click");
+};
+
+$(document).ready(main);
--- Chapter5/Flickr/javascripts/app.js
+++ Chapter5/Flickr/javascripts/app.js
@@ -1,11 +1,28 @@
-var main = function () {
+var main = function () 
+{
 	"use strict";
 
 	var url= "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=dogs&format=json&jsoncallback=?";
 
-	$.getJSON(url, function (flickrResponse){
-		console.log(flickrResponse);
+	$.getJSON(url, function (flickrResponse)
+	{
+		flickrResponse.items.forEach(function (photo)
+		{
+			//create a new jQuery element to hold the image
+			var $img = $("<img>").hide();
+			
+			// set the attribute to the url
+			// contained in the response
+			$img.attr("src", photo.media.m);
+
+			// attach the img tage to the main
+			// photos element
+			$("main .photos").append($img);
+			$img.fadeIn();
+		});
+		
 	});
 
 };
+
 $(document).ready(main); 
