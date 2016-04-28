bb65b590e354d8b8c4581115cd62026c5eb2232d
James Hall (Jim) <jhall20@bucs.fsw.edu>
2016-04-28 18:02:33 -0400

    Update app.js

--- Final/javascripts/app.js
+++ Final/javascripts/app.js
@@ -1,77 +1,50 @@
 var main = function () {
+    "use strict";
 
-     "use strict";
+    var url = "http://jimbarrell.com/fsw/getJSON.php?table=Issues";
+    var $issueRows=$("");
 
- 
+    $.getJSON(url, function (Issues) {
 
-     var url = "http://jimbarrell.com/fsw/getJSON.php?table=Issues";
+        Issues.forEach(function (issue) {
 
-     var $issueRows=$("");
+            $issueRows+="<tr><td>"+
+            issue.ID+"</td><td>" +
+            issue.Title+"</td><td>" +           
+            issue.OpenedBy+"</td><td>" +
+            issue.AssignedTo+"</td><td>" +
+            issue.OpenedDate+"</td><td>" +
+            issue.DueDate+"</td><td>" +
+            issue.Status+"</td><td>" +
+            issue.Category+"</td><td>" +
+            issue.Priority+"</td><td>" +
+            issue.Description+"</td><td>" +
+            issue.RelatedIssue+"</td></tr>";
+        //$list.append($contact);
+        });
+        $("#tblIssues tbody").append($issueRows);
+    });
 
-     $.getJSON(url, function (Issues) {
+    url = "http://jimbarrell.com/fsw/getJSON.php?table=Contacts";
+    var $rows=$("");
 
-          
+    $.getJSON(url, function (Contacts) {
 
-           Issues.forEach(function (issue) {
+        Contacts.forEach(function (contact) {
+            $rows+="<tr><td>"+
+            contact.ID+"</td><td>" +
+            contact.FirstName+"</td><td>" +
+            contact.LastName+"</td><td>" +
+            contact.Company+"</td><td>" +
+            contact.JobTitle+"</td><td>" +
+            '<a href="mailto:'+contact.EmailAddress+'">'+contact.EmailAddress+"</a></td><td>" +
+            contact.BusinessPhone+"</td></tr>";
 
-             $issueRows+="<tr><td>"+ ;
+        //$list.append($contact);
+        });
 
-             issue.ID+"</td><td>" + ;
-
-…
-
-             issue.Description+"</td><td>" +           
-
-             issue.RelatedIssue+"</td><td>&nbsp;</td></tr>";
-
-             //$list.append($contact);
-
-           });
-
-           $("#tblIssues tbody").append($issueRows);
-
-     });
-
- 
-
-    
-
-    
-
-     var url = "http://jimbarrell.com/fsw/getJSON.php?table=Contacts";
-
-     var $rows=$("");
-
-     $.getJSON(url, function (Contacts) {
-
-          
-
-           Contacts.forEach(function (contact) {
-
-             $rows+="<tr><td>"+
-
-             contact.ID+"</td><td>" +
-
-             contact.FirstName+"</td><td>" +
-
-..
-
-             contact.BusinessPhone+"</td></tr><td>&nbsp;</td>";
-
-             //$list.append($contact);
-
-           });
-
-           $("#tblContacts tbody").append($rows);
-
-     });
-
-    
-
- 
-
+        $("#tblContacts tbody").append($rows);
+    });
 };
-
- 
 
 $(document).ready(main);
